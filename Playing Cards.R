7bd277cbf849eb56fc6929a5d7757371978269c7
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-09-26 06:50:24 +0100

    update

--- Playing Cards.R
+++ Playing Cards.R
@@ -23,3 +23,36 @@ shuffle <- function(deck) {
 deal(deck)
 deck_2 <- shuffle(deck)
 deal(deck_2)
+
+deck2 <- deck
+
+deck2$value[deck2$face == 'ace'] <- 14
+
+#learning  how to reassign values
+deck3 <- deck
+deck3$value[deck3$suit == 'hearts'] <- 1
+
+#playimg a game of hearts
+deck4 <- deck
+deck4$value <- 0
+queenOfSpades <- deck4$face == 'queen' & deck4$suit == 'spades'
+
+deck4$value[queenOfSpades] <- 13
+
+#w <- c(-1, 0, 1)
+#x <- c(5, 15)
+#y <- "February"
+#z <- c("Monday", "Tuesday", "Friday")
+
+#w > 0
+#x > 10 & x < 20
+#"y" == y
+#all(z %in% c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
+
+
+#playing a game of blackjack
+deck5 <- deck
+facecard <- deck5$face %in% c("king", "queen", "jack")
+deck5$value[facecard] <- 10
+deck5$value[deck5$face == 'ace'] <- NA
+
